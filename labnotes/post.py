2bcbe03f18e9fd944b9748aaf12a27aba7e7e0f4
Giovanni Doni <6iovanni.doni@gmail.com>
2025-08-19 17:15:20 +0100

    Run every other day

--- labnotes/post.py
+++ labnotes/post.py
@@ -177,7 +177,7 @@ def _main():
     text = get_linkedin_block(data)
 
     # Send to LinkedIn
-    if dt.now().weekday() in [0, 1, 2, 3, 4]:
+    if dt.now().weekday() % 2 == 1:
         post_to_linkedin(text)
 
     logger.info(f"LinkedIn post content generated: {text}")
